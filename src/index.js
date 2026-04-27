598f21d542f3d9a1f7dbb9e8fb65de87ba7c0121
Luis <luanfersapublic@gmail.com>
2026-04-27 00:08:44 -0300

    arreglando webhook whatsapp verify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+const VERIFY_TOKEN = "TOHO2013419598LUANFERSA";
 
-const whatsappRoutes = require("./routes/routes");
+app.get("/whatsapp", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
 
-app.use("/", whatsappRoutes);
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
 
-const PORT = 10000;
+  return res.sendStatus(403);
+});
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Servidor corriendo en puerto " + PORT);
+  console.log("Servidor listo en puerto", PORT);
 });
